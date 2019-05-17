755e53e71cfc282b5439a0dd83aafcd74b6e3200
jakebathman <jake.bathman@gmail.com>
2019-05-17 15:43:52 -0500

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    {{-- Ziggy --}}
+    @routes
+</head>
+<body>
+    <div id="app">
+
+        <main class="py-4">
+            @yield('content')
+            <div id="ziggyInfo" style=" padding:24px;">
+                <h2 style="margin-bottom:1em;">Route name: <strong>{{ \Request::route()->getName() }}</strong></h2>
+
+                <h5 style="">route().current()</h5>
+                <div id="currentRoute" style="display: flex;">
+                    <pre style="padding-left: 42px;"></pre>
+                </div>
+                <h5 style="">Ziggy.namedRoutes</h5>
+                <div id="namedRoutes" style="display: flex;">
+                    <pre style="padding-left: 42px;"></pre>
+                </div>
+            </div>
+            <script>
+                $('#currentRoute pre').html(route().current());
+                $('#namedRoutes pre').html(JSON.stringify(Ziggy.namedRoutes,null,3));
+            </script>
+        </main>
+    </div>
+</body>
+</html>
